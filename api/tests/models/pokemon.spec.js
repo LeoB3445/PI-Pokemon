dd3759c525f438b07c620a4666f23eaf30c85029
Leonardo Bonetto <leonardo1bonetto@protonmail.com>
2022-09-16 17:54:05 -0300

    Created tests for the pokemon model

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -2,20 +2,35 @@ const { Pokemon, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
 describe('Pokemon model', () => {
+  const dummy = {
+    name: "testmon",
+    hp:1,
+    attack:1,
+    speed:1,
+    defense:1,
+    weight:1,
+    height:1
+  }
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
     }));
   describe('Validators', () => {
+    it("should work correctly when all parameters are valid",()=>{
+      Pokemon.create(dummy);
+    })
     beforeEach(() => Pokemon.sync({ force: true }));
     describe('name', () => {
       it('should throw an error if name is null', (done) => {
-        Pokemon.create({})
+        Pokemon.create({...dummy, name:null})
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Pokemon.create({ name: 'Pikachu' });
+      it('should not accept a repeated name', (done) => {
+        Pokemon.create({dummy})
+        .then(()=> Pokemon.create(dummy))
+        .then(()=> done(new Error("should not have accepted repeated name")))
+        .catch(()=>done())
       });
     });
   });
