891e647660589c9577692ef13dc30f668190f9c0
Leonardo Bonetto <leonardo1bonetto@protonmail.com>
2022-09-17 11:07:25 -0300

    created associations test

---
+++ api/tests/models/associations.spec.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const {Pokemon, Type, conn} = require('../../src/db');
+
+describe("Many to Many relation", ()=>{
+    var fakePikachu= Pokemon.build({name:'fakePikachu'});
+    console.log(fakePikachu);
+    var electric = Type.build({name:"electric"})
+    console.log(electric);
+    before(()=> conn.authenticate()
+        .catch(err=>{
+            console.error('Unable to connect to the database:', err);
+        })
+    );
+    beforeEach(()=>{
+        //nota : estas declaraciones hacen que el test sea destructivo.
+        Pokemon.sync({force:true});
+        Type.sync({force:true});
+    });
+    describe("pokemon to type association", ()=>{
+        it("should create associations between defined model instances", ()=>{
+            fakePikachu.addType(electric)
+            .then(()=>{
+                expect(fakePikachu.hasType(electric)).to.equal(true);
+            })
+        })
+    })
+    
+})
