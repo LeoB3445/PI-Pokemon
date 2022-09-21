74414a11e74b38be8441ac3f0d5d200cd44befff
Leonardo Bonetto <leonardo1bonetto@protonmail.com>
2022-09-21 19:53:47 -0300

    Added pokemon creation to /pokemons route

--- api/src/routes/Pokemons.js
+++ api/src/routes/Pokemons.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const { Op } = require('sequelize');
 const {Pokemon, Type} = require('../db');
 
 const pokemons = Router();
@@ -16,6 +17,25 @@ pokemons.get('/',function(req,res){
         .catch(err=>res.status(500).send(err));
     }
 });
+
+pokemons.post('/', function(req, res){
+    var createdMon = Pokemon.create(req.body.pokemon)
+    .then(()=>Type.findAll({
+        where:{
+            name:{
+                [Op.or]:req.body.types
+            }
+        }
+    }))
+    .then((foundTypes)=>{
+        if(foundTypes.length < req.body.types.length){
+            res.status(404).send({msg:`Could not find ${req.body.types.length - foundTypes.length} of the specified types`});
+        }else{
+            return createdMon.setTypes(foundTypes);
+        }
+    })
+    .then(()=> {res.send('Pokemon created successfuly')});
+})
 
 pokemons.get('/:id', function(req,res){
     Pokemon.findByPk(req.params.id)
